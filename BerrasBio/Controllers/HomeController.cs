d2dd34a08349db16f4a6980e94519519e926a4fc
Johanna <32160638+johannaastrom@users.noreply.github.com>
2018-03-19 11:20:16 +0100

    .

--- BerrasBio/Controllers/HomeController.cs
+++ BerrasBio/Controllers/HomeController.cs
@@ -11,25 +11,8 @@ namespace BerrasBio.Controllers
 {
 	public class HomeController : Controller
     {
-		public const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BerrasBio;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
 		public IActionResult Index()
         {
-			using (var conn = new SqlConnection(connectionString))
-			using (var cmd = conn.CreateCommand())
-			{
-				conn.Open();
-				cmd.CommandText = "SELECT FilmTitel, Tid FROM Filmer";
-				/*cmd.Parameters.AddWithValue("@id", id);*/
-				using (var reader = cmd.ExecuteReader())
-				{
-					if (!reader.Read()){
-						//print films 
-						Console.WriteLine("Hallå");
-					}
-				}
-			}
-
 			return View();
         }
 
--- BerrasBio/Models/ErrorViewModel.cs
+++ BerrasBio/Models/ErrorViewModel.cs
@@ -2,11 +2,32 @@ using System;
 
 namespace BerrasBio.Models // här läggs alla modellklasser in
 {
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
+	public class Filmer { }
+	public class Biljetter { }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-		
-    }
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+	}
 }
+
+//public const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BerrasBio;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+//			using (var conn = new SqlConnection(connectionString))
+//			using (var cmd = conn.CreateCommand())
+//			{
+//				conn.Open();
+//				cmd.CommandText = "SELECT FilmTitel, Tid FROM Filmer";
+//				/*cmd.Parameters.AddWithValue("@id", id);*/
+//				using (var reader = cmd.ExecuteReader())
+//				{
+//					if (!reader.Read()){
+//						//print films 
+//						Console.WriteLine("Hallå");
+//					}
+//				}
+//			}
+	
